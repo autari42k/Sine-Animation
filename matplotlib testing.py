d31edd3be11bed3cb963560b8f2b4a378ac0d920
autari42k <lizmarav3@gmail.com>
2026-01-15 10:03:27 -0500

    sympy and matplotlib practice

---
+++ matplotlib testing.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+import random
+
+x_vals = []
+y_vals = []
+
+x = random.randint(0, 10)
+y = random.randint(100, 200)
+
+fig, ax = plt.subplots()
+
+ax.set_title("Ding Dong's Aura")
+ax.set_xlabel("Age")
+ax.set_ylabel("Aura Level")
+ax.grid(True)
+
+
+for i in range(100):
+    
+    x += 2
+    y *= 0.5
+
+    x_vals.append(x)
+    y_vals.append(y)
+
+    ax.plot(x_vals, y_vals, color='blue')
+    
+
+    plt.pause(0.1)
+
+plt.show()
---
+++ sympy testing.py
@@ -0,0 +1,23 @@
+import sympy as smp
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+
+x = smp.symbols('x')
+b = random.randint(1, 5)
+y = smp.sin(x*2) + b
+
+y_f = smp.lambdify(x, y) 
+p = random.randint(1, 60)
+d = 2
+
+for i in range(p):
+    if i % 2 == 0:
+        d += 5
+    x_num = np.linspace(0, i, d)
+    plt.plot(x_num, y_f(x_num), color='black')
+    plt.pause(0.01)
+    print(f'i:{i}, d:{d}')
+
+
+plt.show()
